a2f9f65108ddff348ed4bdaf97ec9b99d277ca6f
ramon <ramon15749@gmail.com>
2018-07-24 15:13:54 +0700

    Ass reset button and user-input grid size

--- script.js
+++ script.js
@@ -3,17 +3,34 @@ function color(){
   console.log("in");
 }
 
-const bigContainer = document.querySelector(".container")
+n = prompt("Enter the dimension");
+size = 960/n;
 
-for(i = 0; i < 16; i++){
+const bigContainer = document.querySelector(".container")
+bigContainer.style.cssText = 'grid-template-columns: repeat(' +n +',1fr)';
+
+for(i = 0; i < n*n; i++){
   const inside = document.createElement('div');
-  inside.textContent = i;
   inside.classList.add('inside');
+  inside.style.cssText = 'width:' + size;
+  inside.style.cssText = 'height:' + size;
   bigContainer.appendChild(inside);
 }
 
+
+
 const insides = document.querySelectorAll('.inside');
+
+function reset(){
+  insides.forEach((inside) => {
+    inside.classList.remove('colored');
+  });
+}
 
 insides.forEach((inside) => {
   inside.addEventListener('mouseover', color)
 });
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', reset);
